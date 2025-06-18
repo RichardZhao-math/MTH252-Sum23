bc209290afa30828c4ae1da7164a6d6e59565002
Richard's Math Stuff <richard.zhao@pcc.edu>
2025-06-18 02:16:29 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -13,37 +13,10 @@ var ptx_lunr_docs = [
   "id": "sec-Strategies-Integration",
   "level": "1",
   "url": "sec-Strategies-Integration.html",
-  "type": "Worksheet",
+  "type": "Section",
   "number": "1.1",
   "title": "Strategies to Evaluate Integrals",
-  "body": " Strategies to Evaluate Integrals   The problem in this activity is inspired by the paper presentating a strategy for indefinite integration by Alan Schoenfeld.  The reference is: Schoenfeld, A. H. (1978). Presenting a strategy for indefinite integration. The American Mathematical Monthly, 85 (8), 673-678.    Back in the 1990s, an instructor placed the following integral at the beginning of a final exam in an integral calculus course:     Imagine you were a student taking this exam. Based on your current knowledge, what is the first method you would try to evaluate this integral? Clearly state your reasoning for choosing this method as your first approach. Then, carry out the integration using this method and show all steps clearly.    Now, come up with a different method that could also be used to evaluate the same integral. Briefly explain why you believe this alternative method is valid and likely to succeed. Then, evaluate the integral using this second method, again showing all steps clearly.   When you finished the above question, expand the solution below and check your answer.   There are actually three methods to solve this integral. They are -sub, trig sub, and particial fraction decomposition.     Using -sub: Let . Then .      Using Trig sub: Let . Then .   where .     Using Partial Fraction: Let and be two numbers. Note that   This implies that   We can determine that and .      "
-},
-{
-  "id": "sec-Strategies-Integration-2",
-  "level": "2",
-  "url": "sec-Strategies-Integration.html#sec-Strategies-Integration-2",
-  "type": "Note",
-  "number": "1.1.1",
-  "title": "",
-  "body": " The problem in this activity is inspired by the paper presentating a strategy for indefinite integration by Alan Schoenfeld.  The reference is: Schoenfeld, A. H. (1978). Presenting a strategy for indefinite integration. The American Mathematical Monthly, 85 (8), 673-678.  "
-},
-{
-  "id": "sec-Strategies-Integration-4",
-  "level": "2",
-  "url": "sec-Strategies-Integration.html#sec-Strategies-Integration-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1",
-  "title": "",
-  "body": " Imagine you were a student taking this exam. Based on your current knowledge, what is the first method you would try to evaluate this integral? Clearly state your reasoning for choosing this method as your first approach. Then, carry out the integration using this method and show all steps clearly.  "
-},
-{
-  "id": "sec-Strategies-Integration-5",
-  "level": "2",
-  "url": "sec-Strategies-Integration.html#sec-Strategies-Integration-5",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2",
-  "title": "",
-  "body": " Now, come up with a different method that could also be used to evaluate the same integral. Briefly explain why you believe this alternative method is valid and likely to succeed. Then, evaluate the integral using this second method, again showing all steps clearly.  "
+  "body": " Strategies to Evaluate Integrals  We have learned a lot of techniques for evaluating integrals in this term.  In this activity, you will explore two specific integrals and reflect on the process.   What's the \"best\" way to evaluate integrals?  Back in the 1990s, an instructor placed the following integral at the beginning of a final exam in an integral calculus course:   Imagine you were a student taking this exam, how would you try evaluating this integral?   First approach to evaluate this integral   What is the first method you would try to evaluate this integral? Clearly state your reasoning for choosing this method as your first approach. Then carry out the integration using this method and show all steps clearly.     Alternative approach to evaluate this integral   Now, come up with a different method that could also be used to evaluate the same integral. Briefly explain why you believe this alternative method is valid and likely to succeed. Then, evaluate the integral using this second method, again showing all steps clearly.     When you finished the above question, expand the solution below and check your answer.   There are (at least) three methods to solve this integral. They are -sub, trig sub, and partial fraction decomposition.     Using -sub: Let . Then .      Using Trig sub: Let . Then .   where .     Using Partial Fraction: Let and be two numbers. Note that   This implies that   We can determine that and .        Compare and reflect   Did your answer(s) match with the solution?      Does calculators evaluate integrals \"smartly\"?  There are some computer algebra system, including WolframAlpha , Symbolab , Integral Calculator , and so many others.   "
 },
 {
   "id": "backmatter-2",
